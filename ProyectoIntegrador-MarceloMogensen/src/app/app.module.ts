e7f308950706931fc42358f14c4e65d743f13393
marcemog <marce.mogensen@gmail.com>
2022-08-18 17:33:06 -0300

    footer listo

--- ProyectoIntegrador-MarceloMogensen/src/app/app.module.ts
+++ ProyectoIntegrador-MarceloMogensen/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { SkillsComponent } from './components/skills/skills.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
